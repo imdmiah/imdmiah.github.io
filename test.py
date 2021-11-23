09a33c7ac68a0f89b8e8ce490a771e9abf184252
Imdad Miah <imdad.miah@imdad.miah>
2021-11-23 15:06:55 +0000

    Initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+function foo(str) {
+    let words ==[];
+
+    for (const word or str.split (" ")) {
+        words.put(word[0].toUpperCase() + word.slice(1))
+    }
+
+    words.join(" ");
+}
+
+console.log(foo("the brown fox jumped over the lazy dog"));
